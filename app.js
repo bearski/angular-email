7517385950a2741014545f546349afbc722b2bdd
bearski <santosh.sewlal@gmail.com>
2017-06-01 15:58:36 +0200

    moved js script to app.js

---
+++ app.js
@@ -0,0 +1,50 @@
+var app = angular.module('emailApp', []);
+
+// app.factory('emails', [function(){
+//   var o = {
+//     posts: []
+//   };
+//   return o;
+// }]);
+
+
+app.controller('emailController', [
+    '$scope',
+    // 'emails',
+    function($scope, emails) {
+        console.log('emailController');
+
+        $scope.test = 'Hello Bear!';
+
+        $scope.isPopupVisible = false;
+
+        $scope.showPopup = function() {
+            $scope.isPopupVisible = true;
+        };
+
+        $scope.emails = [
+            { from: 'John', subject: 'I love angular', date: 'Jan 1' }, 
+            { from: 'Jack', subject: 'Angular and I are just friends', date: 'Feb 15' }, 
+            { from: 'Ember', subject: 'I hate you Angular!', date: 'Dec 8' }
+        ];
+
+		// $scope.emails = emails.emails;
+
+        // $scope.addPost = function() {
+        //     if (!$scope.title || $scope.title === '') {
+        //         return; }
+        //     $scope.posts.push({
+        //         title: $scope.title,
+        //         link: $scope.link,
+        //         upvotes: 0
+        //     });
+        //     $scope.title = '';
+        //     $scope.link = '';
+        // };
+
+        // $scope.incrementUpvotes = function(post) {
+        //     post.upvotes += 1;
+        // };
+    }
+
+]);
